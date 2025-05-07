936cc535b9c651134bce3bb26e6bc45945c152d4
21522337 <21522337@gm.uit.edu.vn>
2025-05-07 22:25:53 +0700

    Giao dien so bo cua game

---
+++ GiaoDienIngame/KeoBuaBao(UI).Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace GiaoDienIngame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            progressBar1 = new ProgressBar();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(498, 421);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.AppWorkspace;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(272, 206);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(0, 3);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(272, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(0, 117);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(272, 92);
+            progressBar1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(pictureBox1);
+            panel2.Location = new Point(516, 12);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(272, 206);
+            panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(progressBar1);
+            panel3.Controls.Add(textBox1);
+            panel3.Location = new Point(516, 224);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(272, 209);
+            panel3.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(21, 290);
+            button1.Name = "button1";
+            button1.Size = new Size(129, 102);
+            button1.TabIndex = 0;
+            button1.Text = "Kéo";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(185, 290);
+            button2.Name = "button2";
+            button2.Size = new Size(129, 102);
+            button2.TabIndex = 1;
+            button2.Text = "Búa";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(353, 290);
+            button3.Name = "button3";
+            button3.Size = new Size(129, 102);
+            button3.TabIndex = 2;
+            button3.Text = "Bao";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 451);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Keo Bua Bao";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private ProgressBar progressBar1;
+        private Panel panel2;
+        private Panel panel3;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+    }
+}
