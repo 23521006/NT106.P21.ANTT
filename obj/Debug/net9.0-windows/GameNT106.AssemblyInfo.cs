79b610c4dbd5ce09b910c975d26fb9c38d5c3890
23521006 <23521006@gm.uit.edu.vn>
2025-07-12 14:51:29 +0700

    fix được một tí logic trong trận

--- obj/Debug/net9.0-windows/GameNT106.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/GameNT106.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameNT106")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc1ed03ae75ddbc52c1c7505ce690342a4cfaa63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b11c4f585c5045ee6afcbd9cecfb8b4533e3ae59")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameNT106")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameNT106")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
